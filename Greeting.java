09535cb5835d64e6f405b7ac62c7be1a58695113
Popov85 <andrey.popov85@gmail.com>
2016-05-20 23:33:50 +0300

    Add files via upload

---
+++ Greeting.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Андрей on 20.05.2016.
+ */
+public class Greeting {
+    public static void main(String[] args) {
+       displayMessage("Hi, I'm your first Java program! Stay tuned!");
+    }
+    private static void displayMessage(String message) {
+        System.out.println(message);
+    }
+
+}
