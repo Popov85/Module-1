9e91a5407fc743988a116604814a2e06a97f9841
Popov85 <andrey.popov85@gmail.com>
2016-05-21 10:10:09 +0300

    Add files via upload
    A little improvement in Greeting class. From now on, it uses displayMessage() twice.

--- Greeting.java
+++ Greeting.java
@@ -1,12 +1,18 @@
+import static java.lang.System.*;
+
 /**
  * Created by Андрей on 20.05.2016.
  */
 public class Greeting {
     public static void main(String[] args) {
-       displayMessage("Hi, I'm your first Java program! Stay tuned!");
+        displayMessage("Hi, I'm your first Java program! ");
+        displayMessage("Stay tuned!");
+
     }
     private static void displayMessage(String message) {
-        System.out.println(message);
+        out.print(message);
+
     }
+
 
 }
