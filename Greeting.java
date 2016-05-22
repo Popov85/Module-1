e995820bd0162885f7ce2ce91b2b7e9b28ba89ab
Andrii Popov <andrey.popov85@gmail.com>
2016-05-22 15:35:50 +0300

    Add files via upload
    Improvements:
    1) "import" directive is deleted.
    2) Tab=8 spaces.
    3) A class within a package.
    4) Unnecessary spaces and indents are deleted.
    5) System.out.println

--- Greeting.java
+++ Greeting.java
@@ -1,19 +1,14 @@
-import static java.lang.System.*;
+package Module1;
 
 /**
  * Created by Андрей on 20.05.2016.
  */
 public class Greeting {
-    public static void main(String[] args) {
-        String progLang="Java";
-        displayMessage("Hi, I'm your first "+ progLang+ " program! ");
-        displayMessage("Stay tuned!");
-
-    }
-    private static void displayMessage(String message) {
-        out.print(message);
-
-    }
-
-
+        public static void main(String[] args) {
+                String programLanguage="Java";
+                displayMessage("Hi, I'm your first "+ programLanguage+ " program! Stay tuned!");
+        }
+        private static void displayMessage(String message) {
+                System.out.println(message);
+        }
 }
